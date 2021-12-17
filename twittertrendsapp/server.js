3af5731a2021bf228806c2377ff7f8088c0f62ae
unknown <raghavgrover5@gmail.com>
2021-12-17 12:15:30 +0000

    Added Twitter account bearer token for authorization

--- twittertrendsapp/server.js
+++ twittertrendsapp/server.js
@@ -4,3 +4,9 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const axios = require("axios");
 const path = require("path");
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAFN6UgEAAAAAKCWEYyaKXipOyTqlktobll7pLO8%3DVp0PiDQALdtK18OEKxM4vDI2O4fQppfQGIfNaVxcVdjiTzcj9p';
