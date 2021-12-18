43173e79b60b5df67e967a0b39753a962189cb20
unknown <raghavgrover5@gmail.com>
2021-12-18 00:02:12 +0000

    Added cors middleware

--- twittertrendsapp/server.js
+++ twittertrendsapp/server.js
@@ -10,3 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAFN6UgEAAAAAKCWEYyaKXipOyTqlktobll7pLO8%3DVp0PiDQALdtK18OEKxM4vDI2O4fQppfQGIfNaVxcVdjiTzcj9p';
+
+// setup middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+if (process.env.NODE_ENV === "development")
+  app.use(cors());
