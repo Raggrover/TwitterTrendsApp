c30fb89bb65b8ea47e74c5f729cb08a8e13cd777
unknown <raghavgrover5@gmail.com>
2021-12-19 13:36:47 +0000

    Updated intendation of files

--- twittertrendsapp/client/src/MainPage.js
+++ twittertrendsapp/client/src/MainPage.js
@@ -5,8 +5,8 @@ const API_ENDPOINT = "/";
 const TRENDS = "api/trends?id="
 const LOCATION_ID = "1"
 
-const trendsReducer = (state,action)=>{
-	switch(action.type){
+const trendsReducer = (state, action) =>  {
+  switch(action.type) {
     case "FETCH_INIT":
       return {
         ...state,
@@ -27,15 +27,14 @@ const trendsReducer = (state,action)=>{
         isError: true
       };
     default:
-      throw new Error();      
-	}
+      throw new Error();
+  }
 };
 
 const List = ({ list }) =>
   list.map((item) => (
     <Item key={item.url} item={item} />
   ));
-
 
 const Item = ({ item }) => {
   return (
@@ -62,17 +61,16 @@ const Item = ({ item }) => {
   );
 };
 
-
 function MainPage() {
-	const [url, setUrl]=useState('${API_ENDPOINT}${TRENDS}${LOCATION_ID}');
-	const [location, setLocation]=useState(LOCATION_ID);
-	const [trends, dispatchTrends]=useReducer(trendsReducer,{
-		data: [],
-		isLoading: true,
-		isError: false
-	});
+  const [url, setUrl] = useState(`${API_ENDPOINT}${TRENDS}${LOCATION_ID}`);
+  const [location, setLocation] = useState(LOCATION_ID);
+  const [trends, dispatchTrends] = useReducer(trendsReducer, {
+    data: [],
+    isLoading: true,
+    isError: false
+  });
 
-	  const handleFormSubmit = (event) => {
+  const handleFormSubmit = (event) => {
     setUrl(`${API_ENDPOINT}${TRENDS}${location}`);
     event.preventDefault();
   }
@@ -93,8 +91,7 @@ function MainPage() {
     }
   }, [url]);
 
-
-    useEffect(() => {
+  useEffect(() => {
     handleFetchTrends();
   }, [handleFetchTrends]);
 
@@ -114,32 +111,33 @@ function MainPage() {
         </div>
       </section>
       <br/>
-    <div className="container">
-    <form onSubmit={handleFormSubmit}>
-      <div className="field has-addons">
-        <div className="control is-expanded">
-          <div className="select is-fullwidth">
-            <select 
-              name="location" 
-              value={location} 
-              onChange={handleChange}
-            >
-              <option value="1">WorldWide</option>
-              <option value="23424848">India</option>
-              <option value="23424977">United States of America</option>
-              <option value="23424975">United Kingdom</option>
-              <option value="23424948">Singapore</option>
-              <option value="23424856">Japan</option>
-            </select>
+      <div className="container">
+        <form onSubmit={handleFormSubmit}>
+          <div className="field has-addons">
+            <div className="control is-expanded">
+              <div className="select is-fullwidth">
+                <select 
+                  name="location" 
+                  value={location} 
+                  onChange={handleChange}
+                >
+                  <option value="1">WorldWide</option>
+                  <option value="23424848">India</option>
+                  <option value="23424977">United States of America</option>
+                  <option value="23424975">United Kingdom</option>
+                  <option value="23424948">Singapore</option>
+                  <option value="23424856">Japan</option>
+                </select>
+              </div>
+            </div>
+            <div className="control">
+              <button type="submit" className="button is-primary">Get Trends</button>
+            </div>
           </div>
-        </div>
-        <div className="control">
-          <button type="submit" className="button is-primary">Get Trends</button>
-        </div>
+        </form>
       </div>
-    </form>
-  </div>
-  <hr/>
+      <hr/>
+      
       {/* {trends.isLoading ? (<p>Loading...</p>) : (console.log(trends.data))} */}
       <div className="container">
         { trends.isError &&
@@ -159,11 +157,9 @@ function MainPage() {
           <List list={trends.data} />
         )}
       </div>
+      
     </div>
-
   );
-
 }
+
 export default MainPage;
-
-
--- twittertrendsapp/server.js
+++ twittertrendsapp/server.js
@@ -17,45 +17,44 @@ app.use(express.json())
 if (process.env.NODE_ENV === "development")
   app.use(cors());
 
-  const authMessage = {
-    title: "Not Authenticated",
-  }
-  
-  app.get("/api/trends", async (req, res) => {
-    if (!BEARER_TOKEN)  {
-      res.status(400).send(authMessage);
-    }
-  
-    const config = {
-      headers: { 'Authorization': `Bearer ${BEARER_TOKEN}` }
-    }
-  
-    try {
-      const response = await axios.get(`https://api.twitter.com/1.1/trends/place.json?id=${req.query.id}`, config);
-   
-      if (response.status !== 200)  {
-        if (response.status === 403)  {
-          res.status(403).send(response.body);
-        } else {
-          throw new Error(response.statusText);
-        }
-      }
-      res.json(response.data);
-    } catch(e) {
-      res.status(404).send(e.response.statusText);
-    }
-  });
-  
-  if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
+const authMessage = {
+  title: "Not Authenticated",
+}
+
+app.get("/api/trends", async (req, res) => {
+  if (!BEARER_TOKEN)  {
+    res.status(400).send(authMessage);
   }
 
-  app.listen(PORT, () =>  {
-    console.log(`Listening at http://localhost:${PORT}`);
-  })
-  
+  const config = {
+    headers: { 'Authorization': `Bearer ${BEARER_TOKEN}` }
+  }
+
+  try {
+    const response = await axios.get(`https://api.twitter.com/1.1/trends/place.json?id=${req.query.id}`, config);
+ 
+    if (response.status !== 200)  {
+      if (response.status === 403)  {
+        res.status(403).send(response.body);
+      } else {
+        throw new Error(response.statusText);
+      }
+    }
+    res.json(response.data);
+  } catch(e) {
+    res.status(404).send(e.response.statusText);
+  }
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(PORT, () =>  {
+  console.log(`Listening at http://localhost:${PORT}`);
+})
